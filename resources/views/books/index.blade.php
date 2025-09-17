b0077285afc16180979bad8b451bad8cc7926671
Dex <a.markdexter@gmail.com>
2025-09-17 11:33:17 +0800

    add search by title

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <h1 class="mb-10 text-2xl">Books</h1>
 
-    <form>
+    <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+        <input class="input h-10" type="text" name="title" placeholder="Search by title" value="{{ request('title') ?? '' }}"/>
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route('books.index') }}" class="h-10 btn">Clear</a>
     </form>
 
     <ul>
